16d66b5cae5b4cf4ebb6f316d97f909e5004a5fe
Carlitos <carlitosdutan@pursuit.org>
2023-11-15 14:45:12 -0500

    characters are displayed and formatted evenly plus borders applied

--- app.js
+++ app.js
@@ -1,28 +1,23 @@
-// fetch(`https://rickandmortyapi.com/api/character/`)
-//   .then((response) => response.json())
-//   .then((data) => data.results.forEach((character) => console.log(character.name)));
+const section = document.querySelector(".character-cards");
 
-fetch('https://rickandmortyapi.com/api/character')
+function displayCharacterCard(character) {
+  const newCard = document.createElement("article")
+  newCard.classList.add("created-card");
+  newCard.innerHTML = `
+  <h2>${character.name}</h2>
+  <img src = "${character.image}" alt = "Image of ${character.name}">
+  <p>Species: ${character.species}</p>
+  <p>Status: ${character.status}</p>
+  <p>Origin: ${character.origin.name}</p>
+  <p>Gender: ${character.gender}</p>
+  <p>Last Seen: ${character.location.name}</p>
+  `
+  section.append(newCard);
+}
+
+fetch("https://rickandmortyapi.com/api/character")
 .then((response) => response.json())
 .then((data) => {
-  const characters = data.results.map((e) => ({
-    name: e.name,
-    species: e.species,
-    status: e.status,
-    origin: e.origin.name,
-    gender: e.gender,
-    lastSeen: e.location.name
-  }));
-  console.log(characters);
-});
-
-
-// .then((data) => data.results.map((e) => console.log(`${e.name} ${e.species} ${e.status} ${e.origin.name} ${e.gender} ${e.location.name}`)))
-
-// .then((data) => data.results.map((e) => {
-//   e.name,
-//   e.species,
-//   e.status,
-//   e.origin.name
-// }));
-// .then((data) => data.results.forEach((e) => ))
+  data.results.forEach((character) => displayCharacterCard(character));
+})
+.catch((error) => console.error("Error fetching data:", error));
