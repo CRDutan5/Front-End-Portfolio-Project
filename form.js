bb9fca8015ab20d28dfdc4c95ee8b131eb8c7ce5
Carlitos <carlitosdutan@pursuit.org>
2023-11-19 20:25:55 -0500

    reset button properly functioning

--- form.js
+++ form.js
@@ -14,9 +14,23 @@ const speciesForm = document.querySelector(".species-selection");
 speciesForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const selectedSpecies = document.querySelector('input[name="option"]:checked');
-    console.log(selectedSpecies.id);
     section.innerHTML = "";
     fetch(`https://rickandmortyapi.com/api/character/?species=${selectedSpecies.id}`)
     .then((response) => response.json())
     .then((data) => data.results.forEach((character) => displayCharacterCard(character)));
 })
+
+const resetButton = document.querySelector(".resetButton");
+resetButton.addEventListener("click", (event) => {
+    event.preventDefault();
+    form.reset();
+    speciesForm.reset();
+    section.innerHTML = ""; 
+    fetch("https://rickandmortyapi.com/api/character")
+        .then((response) => response.json())
+        .then((data) => {
+            data.results.forEach((character) => displayCharacterCard(character));
+        })
+        .catch((error) => console.error("Error fetching data:", error));
+});
+
